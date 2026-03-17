b17350ab209d8fd754d521fed83f6e66fcca17c5
nullbrna <252729190+nullbrna@users.noreply.github.com>
2026-03-17 19:55:54 +0000

    feat(nb): basic working model

--- crates/naive-bayes/src/lib.rs
+++ crates/naive-bayes/src/lib.rs
@@ -1,82 +1,77 @@
-use std::collections::HashMap;
+const NUM_CLASSES: usize = 2;
+const NUM_FEATURES: usize = 4;
 
-/// Data classification. [`Class::Yes`] and [`Class::No`] represent whether
-/// tennis will or won't be played respectively.
-#[derive(Eq, PartialEq, Hash)]
-enum Class {
-    Yes,
-    No,
-}
+/// Discrete values each feature can hold.
+/// Outlook     [0] = sunny  (0) overcast (1) rain (2)
+/// Temperature [1] = hot    (0) mild     (1) cool (2)
+/// Humidity    [2] = high   (0) low      (1)
+/// Wind        [3] = strong (0) weak     (1)
+const FEATURE_SET: [usize; NUM_FEATURES] = [3, 3, 2, 2];
 
-#[derive(Eq, PartialEq, Hash)]
-enum Outlook {
-    Sunny,
-    Overcast,
-    Rain,
-}
-
-#[derive(Eq, PartialEq, Hash)]
-enum Temperature {
-    Hot,
-    Mild,
-    Cold,
-}
-
-#[derive(Eq, PartialEq, Hash)]
-enum Humidity {
-    High,
-    Normal,
-}
-
-#[derive(Eq, PartialEq, Hash)]
-enum Wind {
-    Strong,
-    Weak,
-}
-
-#[derive(Eq, PartialEq, Hash)]
-struct Feature {
-    outlook: Outlook,
-    temperature: Temperature,
-    humidity: Humidity,
-    wind: Wind,
-}
-
-impl Feature {
-    fn new(outlook: Outlook, temperature: Temperature, humidity: Humidity, wind: Wind) -> Self {
-        Self {
-            outlook,
-            temperature,
-            humidity,
-            wind,
-        }
-    }
-}
+/// Maximum number of values any feature can take.
+/// NOTE: Specified for a fixed-size array.
+const MAX_VALUE_COUNT: usize = 3;
 
 #[derive(Default)]
 struct NaiveBayes {
-    /// Occurrences of [`Class`] within dataset.
-    class_counts: HashMap<Class, usize>,
-    /// Occurrences of [`Feature`] within dataset.
-    feature_counts: HashMap<Feature, usize>,
-    /// Training samples.
+    /// Count of samples per-class (0 = No, 1 = Yes).
+    class_occurrences: [usize; NUM_CLASSES],
+    /// Amount of a given value for a feature, per-class.
+    counts: [[[usize; MAX_VALUE_COUNT]; NUM_FEATURES]; NUM_CLASSES],
+    /// Total training samples.
     sample_size: usize,
 }
 
+#[allow(unused)]
 impl NaiveBayes {
-    pub fn train(&mut self, class: Class, feature: Feature) {
+    fn train(&mut self, features: [usize; NUM_FEATURES], class: usize) {
+        // Assume `class` is either 0 or 1. Increment the corresponding class's
+        // and the overall sample count.
+        self.class_occurrences[class] += 1;
         self.sample_size += 1;
 
-        *self.class_counts.entry(class).or_default() += 1;
-        *self.feature_counts.entry(feature).or_default() += 1;
+        // Assume each entry is <= `MAX_VALUE_COUNT` - 1. Increment the given
+        // value for the feature under the given class.
+        for feature in 0..NUM_FEATURES {
+            let value = features[feature];
+            self.counts[class][feature][value] += 1;
+        }
     }
 
-    fn prior(&self, class: Class) -> f64 {
-        self.class_counts
-            .get(&class)
-            .map(usize::to_owned)
-            .unwrap_or_default() as f64
-            / self.sample_size as f64
+    fn predict(&self, features: [usize; NUM_FEATURES]) -> usize {
+        let mut best_class = 0;
+        let mut best_log_probability = f64::NEG_INFINITY;
+
+        for class in 0..NUM_CLASSES {
+            // Base likelihood of this class before seeing any features.
+            let prior = self.class_occurrences[class] as f64 / self.sample_size as f64;
+            // Avoid multiplying probabilities due to underflowing, instead sum
+            // the log of each value.
+            let mut log_probability = prior.ln();
+
+            for feature in 0..NUM_FEATURES {
+                let value = features[feature];
+                // Laplace smoothing. If we come across a value for a feature
+                // that we haven't yet seen for this class, incrementing avoids
+                // a 0 ensuring an "unlikely but possible" prediction.
+                let count = self.counts[class][feature][value] as f64 + 1.0;
+
+                // Add the number of possible values per-feature to the class
+                // occurrences to account for the smoothing above.
+                let total = self.class_occurrences[class] as f64 + FEATURE_SET[feature] as f64;
+
+                log_probability += (count / total).ln();
+            }
+
+            // Compare against the previous class's probabilities. Store the
+            // class weighed with the highest probability from `features`.
+            if log_probability > best_log_probability {
+                best_log_probability = log_probability;
+                best_class = class;
+            }
+        }
+
+        best_class
     }
 }
 
@@ -88,54 +83,50 @@ mod tests {
 
     static MODEL: OnceLock<NaiveBayes> = OnceLock::new();
 
-    fn initialise_model() -> NaiveBayes {
+    fn prepare_model() -> NaiveBayes {
         let mut model = NaiveBayes::default();
 
-        let datasets = vec![
-            (
-                Class::No,
-                Feature::new(Outlook::Sunny, Temperature::Hot, Humidity::High, Wind::Weak),
-            ),
-            (
-                Class::No,
-                Feature::new(
-                    Outlook::Sunny,
-                    Temperature::Hot,
-                    Humidity::High,
-                    Wind::Strong,
-                ),
-            ),
-            (
-                Class::Yes,
-                Feature::new(
-                    Outlook::Overcast,
-                    Temperature::Hot,
-                    Humidity::High,
-                    Wind::Weak,
-                ),
-            ),
-            (
-                Class::Yes,
-                Feature::new(Outlook::Rain, Temperature::Mild, Humidity::High, Wind::Weak),
-            ),
+        // Dataset for weather conditions determining whether or not a game of
+        // tennis is played. See the key commented above `FEATURE_SET` for more.
+        let dataset = [
+            (0, [0, 0, 0, 0]),
+            (0, [0, 0, 0, 1]),
+            (1, [1, 0, 0, 0]),
+            (1, [2, 1, 0, 0]),
+            (1, [2, 2, 1, 0]),
+            (0, [2, 2, 1, 1]),
+            (1, [1, 2, 1, 1]),
+            (0, [0, 1, 0, 0]),
+            (1, [0, 2, 1, 0]),
+            (1, [2, 1, 1, 0]),
+            (1, [0, 1, 1, 1]),
+            (1, [1, 1, 0, 1]),
+            (1, [1, 0, 1, 0]),
+            (0, [2, 1, 0, 1]),
         ];
 
-        for pair in datasets {
-            model.train(pair.0, pair.1)
+        for (class, features) in dataset {
+            model.train(features, class);
         }
 
         model
     }
 
     #[test]
-    fn nb_no_for_sunny_mild_high_strong() {
-        let model = MODEL.get_or_init(initialise_model);
+    fn nb_yes_for_overcast_cool_low_weak() {
+        let model = MODEL.get_or_init(prepare_model);
 
-        let feature = Feature::new(
-            Outlook::Sunny,
-            Temperature::Mild,
-            Humidity::High,
-            Wind::Strong,
-        );
+        // NOTE: Overcast is always in the "Yes" class.
+        assert_eq!(model.predict([1, 2, 1, 1]), 1);
+    }
+
+    #[test]
+    fn nb_no_for_rain_mild_high_weak() {
+        let model = MODEL.get_or_init(prepare_model);
+
+        // NOTE: The model could technically predict a "Yes", although this
+        // exact feature list and class is within the dataset. An aggregated
+        // score from the other sets could outweigh this result.
+        assert_eq!(model.predict([2, 1, 0, 1]), 0);
     }
 }
